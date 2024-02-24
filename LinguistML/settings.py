8be01a03ca9edb69a5bd0a4d0c1ab3cc60dbc409
Tolga Özgün <tolgaozgunn@gmail.com>
2024-02-24 20:42:16 +0300

    Fix static image path

--- LinguistML/settings.py
+++ LinguistML/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATIC_URL = '/static/'
+STATIC_URL = env("BASE_URL_PREFIX") + '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = 'media'
