5ed7e77c73e41c3c1e9821db9422792d27842368
Tolga Özgün <tolgaozgunn@gmail.com>
2024-02-24 20:44:05 +0300

    Fix Docker setup

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,17 @@
+# Dockerfile
+
+# The first instruction is what image we want to base our container on
+# We Use an official Python runtime as a parent image
+FROM python:3.10
+
+# Allows docker to cache installed dependencies between builds
+COPY requirements.txt requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
 # After copying your project files
 COPY . /usr/src/app/
+
+WORKDIR /usr/src/app
 
 # Copy the entrypoint script
 COPY entrypoint.sh /usr/src/app/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,17 +7,31 @@ DB_USER=${DB_USER:-user}
 DB_PASSWORD=${DB_PASSWORD:-password}
 DB_NAME=${DB_NAME:-mydatabase}
 
+timestamp() {
+  date +"%T" # current time
+}
+
 # Example of waiting for PostgreSQL to become available (adjust as needed for your DB)
-until PGPASSWORD=$DB_PASSWORD pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER; do
-  echo "Waiting for PostgreSQL at $DB_HOST:$DB_PORT to become available..."
-  sleep 2
-done
+# until PGPASSWORD=$DB_PASSWORD pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER; do
+#   timestamp
+#   echo "Waiting for PostgreSQL at $DB_HOST:$DB_PORT to become available..."
+#   sleep 2
+# done
+
+timestamp
 echo "PostgreSQL is available"
 
 # Apply database migrations
+timestamp
 echo "Applying database migrations..."
 python manage.py makemigrations
 python manage.py migrate
 
+timestamp
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 # Start your Django app
+
+timestamp
+echo "Entry point script executed successfully"
 exec "$@"
