cc7b863cd82c71db1da4e53ec0eb4f230f9d50fc
Tolga Özgün <tolgaozgunn@gmail.com>
2024-04-03 22:23:35 +0300

    Add delete message and delete conversation endpoints

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from chat.views.clear_conversation import clear_conversation
 from chat.views.create_conversation import create_conversation
 
+from chat.views.delete_conversation import delete_conversation
+from chat.views.delete_message import delete_message
 from chat.views.generate_chat_response import generate_chat_response
 from chat.views.get_conversation_messages import get_last_conversation_messages
 from chat.views.get_all_conversation_messages import get_all_conversation_messages
@@ -13,6 +16,9 @@ urlpatterns = [
     path('chat/send/<str:conversation_id>', generate_chat_response),
     path('chat/all/<str:conversation_id>', get_all_conversation_messages),
     path('chat/latest/<str:conversation_id>', get_last_conversation_messages),
+    path('chat/delete/<str:message_id>', delete_message),
+    path('delete/<str:conversation_id>', delete_conversation),
+    path('clear/<str:conversation_id>', clear_conversation),
     path("create", create_conversation, name="create_conversation"),
     path("user", get_user_conversations, name="get_user_conversations"),
     path("bots", get_all_bots),
---
+++ chat/views/delete_message.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from datetime import datetime
+from chat.models import Conversation, Message
+from chat.prompts.chat_prompt import get_chat_prompt
+from constants.header_constants import HEADER_USER_EMAIL
+from profiling.models import Profile
+from profiling.tasks.update_profile import update_profile_async
+
+from utils.http_utils import generate_error_response, generate_success_response
+from utils.gemini_utils import gemini_model
+from drf_yasg.utils import swagger_auto_schema
+import time
+from constants.profile_constants import MAX_NO_OF_MESSAGE_CONTEXT
+from concurrent.futures import ThreadPoolExecutor
+
+
+from drf_yasg import openapi
+
+@swagger_auto_schema(
+    method="delete",
+    operation_description="Delete a message from a conversation",
+    operation_id="Delete a message from a conversation",
+    operation_summary="Delete a message from a conversation",
+    manual_parameters=[
+        openapi.Parameter(
+            name='message_id', in_=openapi.IN_PATH,
+            description='Unique identifier for the message',
+            type=openapi.TYPE_STRING,
+            required=True
+        )
+    ],
+    responses={
+        "200": openapi.Response(
+            description="Message deleted successfully",
+            examples={
+                "application/json": {
+                    "timestamp": "2021-08-30 14:00:00",
+                    "status": 200,
+                    "msg": "Message deleted successfully",
+                    "data": "Message ID here..."
+                }
+            }
+        ),
+        "400": openapi.Response(
+            description="Bad request",
+            examples={
+                "application/json": {
+                    "timestamp": "2021-08-30 14:00:00",
+                    "status": 400,
+                    "msg": "Authentication is required"
+                }
+            }
+        )
+    }
+)
+@api_view(['DELETE'])
+def delete_message(request, message_id: str):
+    
+    # Check the request header for email
+    if not request.headers or HEADER_USER_EMAIL not in request.headers:
+        return generate_error_response(400, "Authentication is required")
+    
+    email = request.headers.get(HEADER_USER_EMAIL, None)
+    if not email:
+        return generate_error_response(400, "Authentication is required")
+    
+    if not message_id:
+        return generate_error_response(400, "Message ID is required")
+    
+    message_exists = Message.objects.filter(id=message_id).exists()
+    
+    # Now get the last five messages from the conversations
+    if not message_exists:
+        return generate_error_response(400, "Message does not exist")
+    
+    message = Message.objects.get(id=message_id)
+    
+    message.delete()
+    
+    return generate_success_response("Message deleted successfully", message_id)
