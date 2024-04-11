aa65214923c8dfa6082f0932ed2a827c90a8c025
Tolga Özgün <tolgaozgunn@gmail.com>
2024-04-11 18:05:07 +0300

    Fix .gitignore and add UnknownWord model to chat/models.py

--- chat/models.py
+++ chat/models.py
@@ -2,6 +2,19 @@ import uuid
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
+class UnknownWord(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    updatedDate = models.DateTimeField(auto_now=True)
+    word = models.CharField(max_length=255)
+    conversation = models.ForeignKey('Conversation', on_delete=models.CASCADE, related_name='unknownWords', null=True)
+    isActive = models.BooleanField(default=True)
+    confidenceLevel = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    
+    def __str__(self):
+        return "UnknownWord: " + self.word
+
 class ChatBot(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     createdDate = models.DateTimeField(auto_now_add=True)
@@ -23,6 +36,7 @@ class Conversation(models.Model):
     userEmail = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     bot = models.ForeignKey(ChatBot, on_delete=models.CASCADE, related_name='conversations')
+    unknownWords = models.ManyToManyField(UnknownWord, related_name='conversations')
     
     def __str__(self):
         return f"Conversation: {self.title}"    
@@ -39,15 +53,3 @@ class Message(models.Model):
     
     def __str__(self):
         return f"{self.senderType}: {self.messageText}"
-    
-
-chatbot_output_example = {
-    "id": "123e4567-e89b-12d3-a456-426614174000",
-    "created_date": "2021-08-30 14:00:00",
-    "updated_date": "2021-08-30 14:00:00",
-    "name": "Test Chatbot",
-    "description": "This is a test chatbot",
-    "profile_image": "test_image.png",
-    "voice_characteristics": "British",
-    "difficulty_level": 10,
-}
