2c5f3b2e8aa7590d1859ac5c634160cf64558a15
Tolga Özgün <tolgaozgunn@gmail.com>
2024-04-18 20:59:17 +0300

    Update confidence level range and sentence scoring

--- constants/unknown_word_constants.py
+++ constants/unknown_word_constants.py
@@ -20,3 +20,17 @@ MCQ_TEST_QUESTION_PER_WORD = 1
 
 INCREASE_CONFIDENCE_ON_CORRECT_MCQ_ANSWER = 1
 DECREASE_CONFIDENCE_ON_WRONG_MCQ_ANSWER = 1
+
+# For a total score out of 5
+# Score: 1 -> decrease confidence by 2
+# Score: 2 -> decrease confidence by 1
+# Score: 3 -> no change
+# Score: 4 -> increase confidence by 1
+# Score: 5 -> increase confidence by 2
+CHANGE_CONFIDENCE_ON_SENTENCE_SCORING = [
+    -2,
+    -1,
+    0,
+    1,
+    2
+]
