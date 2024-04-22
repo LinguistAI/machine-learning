67dd987f1032f281123db3dcd6225b2600b53c94
Tolga Özgün <tolgaozgunn@gmail.com>
2024-04-22 14:35:07 +0300

    Update Conversation serializer

--- chat/serializers.py
+++ chat/serializers.py
@@ -2,11 +2,58 @@
 
 from chat.models import ChatBot, Conversation, Message, UnknownWord
 from rest_framework import serializers
+from drf_yasg import openapi
 
 class UnknownWordSerializer(serializers.ModelSerializer):
     class Meta:
         model = UnknownWord
         fields = '__all__'
+        swagger_schema_fields = {
+            "type": openapi.TYPE_OBJECT,
+            "title": "Unknown Word",
+            "properties": {
+                "id": openapi.Schema(
+                    title="ID",
+                    type=openapi.TYPE_STRING,
+                    example="d4b6b3b0-0b3b-4b3b-8b3b-0b3b4b3b4b3b",
+                ),
+                "listId": openapi.Schema(
+                    title="List ID",
+                    type=openapi.TYPE_STRING,
+                    example="d4b6b3b0-0b3b-4b3b-8b3b-0b3b4b3b4b3b",
+                ),
+                "email": openapi.Schema(
+                    title="Email",
+                    type=openapi.TYPE_STRING,
+                    example="email@email.com",
+                ),
+                "word": openapi.Schema(
+                    title="Word",
+                    type=openapi.TYPE_STRING,
+                    example="linguist",
+                ),
+                "createdDate": openapi.Schema(
+                    title="Created Date",
+                    type=openapi.TYPE_STRING,
+                    example="2021-08-30 14:00:00",
+                ),
+                "updatedDate": openapi.Schema(
+                    title="Updated Date",
+                    type=openapi.TYPE_STRING,
+                    example="2021-08-30 14:00:00",
+                ),
+                "isActive": openapi.Schema(
+                    title="Is Active",
+                    type=openapi.TYPE_BOOLEAN,
+                    example=True,
+                ),
+                "confidenceLevel": openapi.Schema(
+                    title="Confidence Level",
+                    type=openapi.TYPE_NUMBER,
+                    example=23.2,
+                ),
+            }
+        }
         
         
 class MessageSerializer(serializers.ModelSerializer):
@@ -23,7 +70,9 @@ class ChatBotSerializer(serializers.ModelSerializer):
 
 class ConversationSerializer(serializers.ModelSerializer):
     bot = ChatBotSerializer(read_only=True)  # Use the ChatBotSerializer for the 'bot' field
+    unknownWords = UnknownWordSerializer(many=True, read_only=True)  # Use the UnknownWordSerializer for the 'unknownWords' field
 
     class Meta:
         model = Conversation
-        fields = '__all__'
+        fields = ['id', 'createdDate', 'updatedDate', 'userEmail', 'bot', 'unknownWords']
+        
--- chat/views/clear_conversation.py
+++ chat/views/clear_conversation.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from datetime import datetime
 from chat.models import Conversation, Message
 from chat.prompts.chat_prompt import get_chat_prompt
+from chat.serializers import ConversationSerializer
 from constants.header_constants import HEADER_USER_EMAIL
 from profiling.models import Profile
 from profiling.tasks.update_profile import update_profile_async
@@ -39,7 +40,76 @@ from drf_yasg import openapi
                     "timestamp": "2021-08-30 14:00:00",
                     "status": 200,
                     "msg": "Conversation messages cleared successfully",
-                    "data": "Conversation ID here..."
+                    "data": [
+                        {
+                            "id": "6ca7f806-a054-4b78-848b-0bf9da1885e2",
+                            "createdDate": "2024-04-18T09:29:36.556201Z",
+                            "updatedDate": "2024-04-18T09:42:47.071779Z",
+                            "userEmail": "mehmet_dogu123@hotmail.com",
+                            "bot": {
+                                "id": "37ed45c3-e029-4769-b5de-03ded8a5abdf",
+                                "createdDate": "2024-02-26T21:45:49.031000Z",
+                                "updatedDate": "2024-02-26T22:00:14.073000Z",
+                                "name": "Albert Einstein",
+                                "description": "Albert Einstein, the iconic theoretical physicist known for his intelligence and wit, now serves as a mentor in the world of language learning. With his wild hair and kind eyes, he explains complex ideas in simple, relatable terms, inspiring learners to explore the universe of languages with curiosity and perseverance.",
+                                "profileImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Albert_Einstein_1947.jpg/1200px-Albert_Einstein_1947.jpg",
+                                "voiceCharacteristics": "istebububu",
+                                "difficultyLevel": 50
+                            },
+                            "unknownWords": [
+                                {
+                                    "id": "d278ba68-8d01-46a5-8e42-a23b79bfecde",
+                                    "listId": "0165ea1d-54f4-4b52-9578-2cdc86b7ec9b",
+                                    "email": "mehmet_dogu123@hotmail.com",
+                                    "createdDate": "2024-04-18T09:42:46.986405Z",
+                                    "updatedDate": "2024-04-18T09:42:46.995996Z",
+                                    "word": "clean",
+                                    "isActive": True,
+                                    "confidenceLevel": 1.0
+                                },
+                                {
+                                    "id": "5d4abaaf-80d7-4aef-88f5-9bbc949785da",
+                                    "listId": "0165ea1d-54f4-4b52-9578-2cdc86b7ec9b",
+                                    "email": "mehmet_dogu123@hotmail.com",
+                                    "createdDate": "2024-04-18T09:42:47.032461Z",
+                                    "updatedDate": "2024-04-18T09:42:47.034285Z",
+                                    "word": "strategy",
+                                    "isActive": True,
+                                    "confidenceLevel": 1.0
+                                },
+                                {
+                                    "id": "dc90fb08-9649-4166-98e7-75cd6400a20d",
+                                    "listId": "0165ea1d-54f4-4b52-9578-2cdc86b7ec9b",
+                                    "email": "mehmet_dogu123@hotmail.com",
+                                    "createdDate": "2024-04-18T09:42:47.043687Z",
+                                    "updatedDate": "2024-04-18T09:42:47.044963Z",
+                                    "word": "application",
+                                    "isActive": True,
+                                    "confidenceLevel": 1.0
+                                },
+                                {
+                                    "id": "3f4b7fee-ca45-4efc-9bb6-6a7209996b01",
+                                    "listId": "0165ea1d-54f4-4b52-9578-2cdc86b7ec9b",
+                                    "email": "mehmet_dogu123@hotmail.com",
+                                    "createdDate": "2024-04-18T09:42:47.053654Z",
+                                    "updatedDate": "2024-04-18T09:42:47.055037Z",
+                                    "word": "school",
+                                    "isActive": True,
+                                    "confidenceLevel": 1.0
+                                },
+                                {
+                                    "id": "45f9e2fc-449c-4acf-9885-5a0c54e00c9a",
+                                    "listId": "0165ea1d-54f4-4b52-9578-2cdc86b7ec9b",
+                                    "email": "mehmet_dogu123@hotmail.com",
+                                    "createdDate": "2024-04-18T09:42:47.063605Z",
+                                    "updatedDate": "2024-04-18T09:42:47.064992Z",
+                                    "word": "work",
+                                    "isActive": True,
+                                    "confidenceLevel": 1.0
+                                }
+                            ]
+                        }
+                    ]
                 }
             }
         ),
@@ -92,4 +162,8 @@ def clear_conversation(request, conversation_id: str):
     
     messages.delete()
     
-    return generate_success_response("Conversation messages cleared successfully", conversation_id)
+    conversation = Conversation.objects.filter(id=conversation_id).first()
+    
+    serializer = ConversationSerializer(conversation)
+    
+    return generate_success_response("Conversation messages cleared successfully", serializer.data)
