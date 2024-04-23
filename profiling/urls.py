9d1123d10d859c16d15e131935e5bf6fde8bdbd5
KardelenCeren <kceren199@gmail.com>
2024-04-23 10:50:27 +0300

    feat(LINGUIST-414): ML proifle update endpoint

--- profiling/urls.py
+++ profiling/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 
 from profiling.views.get_user_profile import get_user_profile
 from profiling.views.update_user_profile import update_user_profile
+from profiling.views.update_ml_profile import update_ml_profile
 
 urlpatterns = [
     path('user', get_user_profile),
-    path('update', update_user_profile)
+    path('update', update_user_profile),
+    path('update-ml', update_ml_profile)
 ]
