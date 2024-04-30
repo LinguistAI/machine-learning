4e3a4eb5550d90b68e19df78a479ffcabc839f89
Tolga Özgün <tolgaozgunn@gmail.com>
2024-04-30 12:56:29 +0300

    Remove lastMessageId param and add page param to conversation messages

--- chat/views/MessageListView.py
+++ chat/views/MessageListView.py
@@ -14,24 +14,14 @@ class MessageListView(ListAPIView):
     permission_classes = [IsAuthenticatedByHeader]
     pagination_class = DefaultPagination
 
-    
-
     def get_queryset(self):
         conversation_id = self.kwargs.get('conversation_id')
-        last_message_id = self.request.query_params.get('lastMessageId')
-        
-        print("conversation_id: ", conversation_id)
-        print("last_message_id: ", last_message_id)
 
         # Fetch the specified conversation
         conversation = get_object_or_404(Conversation, id=conversation_id)
 
         # Build the queryset for messages
-        queryset = Message.objects.filter(conversation=conversation).order_by('-createdDate')
+        queryset = Message.objects.filter(
+            conversation=conversation).order_by('-createdDate')
 
-        if last_message_id:
-            # Get the 'createdDate' of the message specified by 'lastMessageId'
-            last_message = get_object_or_404(Message, id=last_message_id)
-            queryset = queryset.filter(createdDate__lt=last_message.createdDate)
-        
         return queryset
