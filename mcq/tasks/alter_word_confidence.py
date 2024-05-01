ef3f99d998455ea9ad16b9ed410c04ee735b3a0a
Tolga Özgün <tolgaozgunn@gmail.com>
2024-05-01 14:28:18 +0300

    Add null listId support for ghost list

--- mcq/tasks/alter_word_confidence.py
+++ mcq/tasks/alter_word_confidence.py
@@ -8,36 +8,49 @@ from mcq.tasks.request_decrease_confidence_backend import request_decrease_confi
 from mcq.tasks.request_increase_confidence_backend import request_increase_confidence_backend
 
 
-def increase_word_confidence(email: str, 
+def increase_word_confidence(email: str,
                              unknown_word: UnknownWord,
                              increase_amount: float):
-    
+
+    if unknown_word.listId == None or unknown_word.listId == "":
+        return
+
     previous_confidence = unknown_word.confidenceLevel
     next_confidence = previous_confidence + increase_amount
-    
-    previous_confidence_category = floor(previous_confidence / CONFIDENCE_LEVEL_SCALING_FACTOR)
-    next_confidence_category = floor(next_confidence / CONFIDENCE_LEVEL_SCALING_FACTOR)
+
+    previous_confidence_category = floor(
+        previous_confidence / CONFIDENCE_LEVEL_SCALING_FACTOR)
+    next_confidence_category = floor(
+        next_confidence / CONFIDENCE_LEVEL_SCALING_FACTOR)
     # Increase confidence in current db
     unknown_word.increase_confidence(increase_amount)
-    
+
     if previous_confidence_category != next_confidence_category:
         # Update user service to increase confidence async
         executor = ThreadPoolExecutor()
-        executor.submit(request_increase_confidence_backend, email, unknown_word)
-    
-def decrease_word_confidence(email: str, 
+        executor.submit(request_increase_confidence_backend,
+                        email, unknown_word)
+
+
+def decrease_word_confidence(email: str,
                              unknown_word: UnknownWord,
                              decrease_amount: float):
-    
+
+    if unknown_word.listId == None or unknown_word.listId == "":
+        return
+
     previous_confidence = unknown_word.confidenceLevel
     next_confidence = previous_confidence - decrease_amount
-    
-    previous_confidence_category = floor(previous_confidence / CONFIDENCE_LEVEL_SCALING_FACTOR)
-    next_confidence_category = floor(next_confidence / CONFIDENCE_LEVEL_SCALING_FACTOR)
+
+    previous_confidence_category = floor(
+        previous_confidence / CONFIDENCE_LEVEL_SCALING_FACTOR)
+    next_confidence_category = floor(
+        next_confidence / CONFIDENCE_LEVEL_SCALING_FACTOR)
     # Increase confidence in current db
     unknown_word.decrease_confidence(decrease_amount)
-    
+
     if previous_confidence_category != next_confidence_category:
         # Update user service to increase confidence async
         executor = ThreadPoolExecutor()
-        executor.submit(request_decrease_confidence_backend, email, unknown_word)
+        executor.submit(request_decrease_confidence_backend,
+                        email, unknown_word)
