d46211459bac8bf9c1a991e70ba6d8fa25d12f27
Yagiz Can Aslan <can.aslan@ug.bilkent.edu.tr>
2024-05-14 23:23:53 +0300

    feat(LINGUIST-590): Added spanish prompt to generate_chat_response

--- chat/models.py
+++ chat/models.py
@@ -14,14 +14,14 @@ class UnknownWord(models.Model):
     # Check unknown_word_constants.py for ML_CONFIDENCE_LEVEL_MAX value, it should be the max value
     # It is not directly set here, as that value changes db migration will be required and service will be halted.
     confidenceLevel = models.FloatField(default=1.0, validators=[MinValueValidator(1), MaxValueValidator(100)])
-    
+
     def __str__(self):
         return "UnknownWord: " + self.word
-    
+
     def increase_confidence(self, amount):
         self.confidenceLevel = min(self.confidenceLevel + amount, 100)
         self.save()
-        
+
     def decrease_confidence(self, amount):
         self.confidenceLevel = max(self.confidenceLevel - amount, 1)
         self.save()
@@ -35,8 +35,9 @@ class ChatBot(models.Model):
     profileImage = models.TextField()
     prompt = models.TextField()
     voiceCharacteristics = models.TextField()
+    language = models.TextField('ENG')
     difficultyLevel = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
-    
+
     def __str__(self):
         return "ChatBot: " + self.name
 
@@ -50,10 +51,10 @@ class Conversation(models.Model):
     unknownWords = models.ManyToManyField(UnknownWord, related_name='conversations')
     lastMessage = models.TextField(default="")
     update_words = models.BooleanField(default=True)
-    
+
     def __str__(self):
-        return f"Conversation: {self.title}"    
-    
+        return f"Conversation: {self.title}"
+
 
 class Message(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -63,6 +64,6 @@ class Message(models.Model):
     updatedDate = models.DateTimeField(auto_now=True)
     messageText = models.TextField()
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-    
+
     def __str__(self):
         return f"{self.senderType}: {self.messageText}"
